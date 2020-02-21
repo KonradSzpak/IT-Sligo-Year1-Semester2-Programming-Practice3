138b2e6c1bdeca0c3fcf76355675a2c724f328a9
Konrad Szpak <S00197298@mail.itsligo.ie>
2020-02-21 09:44:54 +0000

    Add project files.

---
+++ lab9/lab9q1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] output = new int[1000];
+
+            for (int i = 0; i < 999; i++)
+            {
+                output[i] = i;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ lab9q2/lab9q2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] output = new int[1000];
+
+            for (int i = 0; i < 999; i=i+3)
+            {
+                
+                output[i] = i;
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
