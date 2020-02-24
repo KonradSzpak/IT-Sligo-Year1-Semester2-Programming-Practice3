7ad6227896290819bb8295dd3a859157e2a291d6
Konrad Szpak <konradszpak67@gmail.com>
2020-02-24 09:17:35 +0000

    test

---
+++ justTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace justTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
